b08ec59573dc73ee030969484131c9838a7ddfed
MarkoKillin <mirkovicmarko790@gmail.com>
2024-05-25 16:42:02 +0200

    Added method to PlantedPartition

--- src/modelgen/PlantedPartition.java
+++ src/modelgen/PlantedPartition.java
@@ -6,7 +6,7 @@ public class PlantedPartition<V, E> {
 
     public UndirectedSparseGraph<V, E> generatePP(int n, double z, double p, double q, Class<V> vClass, Class<E> eClass) {
         UndirectedSparseGraph<V, E> graph = new UndirectedSparseGraph<>();
-
+            int i = graph.getVertexCount();
         return graph;
     }
 }
