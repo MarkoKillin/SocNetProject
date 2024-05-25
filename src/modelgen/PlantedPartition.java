e924737eb69c507feb8d03a785c503395705b7c2
MarkoKillin <mirkovicmarko790@gmail.com>
2024-05-25 16:40:29 +0200

    Added method to Plan

--- src/modelgen/PlantedPartition.java
+++ src/modelgen/PlantedPartition.java
@@ -1,4 +1,12 @@
 package modelgen;
 
-public class PlantedPartition {
+import edu.uci.ics.jung.graph.UndirectedSparseGraph;
+
+public class PlantedPartition<V, E> {
+
+    public UndirectedSparseGraph<V, E> generatePP(int n, double z, double p, double q, Class<V> vClass, Class<E> eClass) {
+        UndirectedSparseGraph<V, E> graph = new UndirectedSparseGraph<>();
+
+        return graph;
+    }
 }
