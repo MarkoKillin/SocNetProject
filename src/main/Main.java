1885cd9a10c83459e1091605d0338f54ded1cca9
MarkoKillin <mirkovicmarko790@gmail.com>
2024-06-13 22:17:13 +0200

    Running metrics

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import edu.uci.ics.jung.graph.UndirectedSparseGraph;
 import graphio.GraphLoader;
 import metrics.GraphMetrics;
+import modelgen.BarabasiAlbert;
 import modelgen.ErdosRenyi;
 
 public class Main {
@@ -16,13 +17,49 @@ public class Main {
 //        m.calculateMetrics(graph, "LastFmAsia");
 //        System.out.println("done in ---- " + (System.currentTimeMillis() - time)/1000.0);
 
-//        erdosrenyi network
+//        second network
+//        long time = System.currentTimeMillis();
+//        GraphLoader gl = new GraphLoader();
+//        UndirectedSparseGraph<Integer, String> graph = gl.loadHighEnergyPhysicsColaboration();
+//        System.out.println("generated in ---- " + (System.currentTimeMillis() - time)/1000.0);
+//        GraphMetrics m = new GraphMetrics();
+//        m.calculateMetrics(graph, "Physics");
+//        System.out.println("done in ---- " + (System.currentTimeMillis() - time)/1000.0);
+
+//        third network not completed
+//        long time = System.currentTimeMillis();
+//        GraphLoader gl = new GraphLoader();
+//        UndirectedSparseGraph<Integer, String> graph = gl.loadAstroPh();
+//        System.out.println("generated in ---- " + (System.currentTimeMillis() - time)/1000.0);
+//        GraphMetrics m = new GraphMetrics();
+//        m.calculateMetrics(graph, "AstroPhysics");
+//        System.out.println("done in ---- " + (System.currentTimeMillis() - time)/1000.0);
+
+//        fourth network
+        long time = System.currentTimeMillis();
+        GraphLoader gl = new GraphLoader();
+        UndirectedSparseGraph<Integer, String> graph = gl.loadDBLP();
+        System.out.println("generated in ---- " + (System.currentTimeMillis() - time)/1000.0);
+        GraphMetrics m = new GraphMetrics();
+        m.calculateMetrics(graph, "DBLP");
+        System.out.println("done in ---- " + (System.currentTimeMillis() - time)/1000.0);
+
+//        erdos-renyi network
 //        long time = System.currentTimeMillis();
 //        ErdosRenyi<Integer, String> er = new ErdosRenyi<>();
-//        UndirectedSparseGraph<Integer, String> graph = er.generateER(1000, 0.001, i -> i, s -> s);
+//        UndirectedSparseGraph<Integer, String> graph = er.generateER(5000, 0.001, i -> i, s -> s);
 //        System.out.println("generated in ---- " + (System.currentTimeMillis() - time)/1000.0);
 //        GraphMetrics m = new GraphMetrics();
 //        m.calculateMetrics(graph, "ErdosRenyi5k");
+//        System.out.println("done in ---- " + (System.currentTimeMillis() - time)/1000.0);
+
+//          barabasi-albert network
+//        long time = System.currentTimeMillis();
+//        BarabasiAlbert<Integer, String> ba = new BarabasiAlbert<>();
+//        UndirectedSparseGraph<Integer, String> graph = ba.generateBA();
+//        System.out.println("generated in ---- " + (System.currentTimeMillis() - time)/1000.0);
+//        GraphMetrics m = new GraphMetrics();
+//        m.calculateMetrics(graph, "BarabashiAlbert");
 //        System.out.println("done in ---- " + (System.currentTimeMillis() - time)/1000.0);
     }
 }
